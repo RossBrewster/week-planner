c3060519d1663992245e5d3ed6eddd9e55a4bffb
Bug Smith <salttyyy@gmail.com>
2023-09-22 21:15:12 +0000

    completed 11

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const $addEvent = document.querySelector('.add-event');
 const $overlay = document.querySelector('.overlay');
 const $modalCancel = document.querySelector('#modal-cancel');
-const $modalTime = document.querySelector('#event-time option:checked');
+const $modalTime = document.querySelector('#event-time');
 const $tableBody = document.querySelector('#table-body');
 const $modalEventInfo = document.querySelector('#event-info');
 const $modalForm = document.querySelector('.add-event-form');
@@ -30,11 +30,13 @@ function renderEntry() {
   const $pTwo = document.createElement('p');
   const $textContentTwo = document.createTextNode($modalEventInfo.value);
   const $tdThree = document.createElement('td');
+  const $row = document.createElement('div');
   const $buttonEdit = document.createElement('button');
   const $edit = document.createTextNode('EDIT');
   const $buttonDelete = document.createElement('button');
   const $delete = document.createTextNode('DELETE');
 
+  $row.className = 'row justify-around';
   $buttonEdit.setAttribute('class', 'edit-button');
   $buttonDelete.setAttribute('class', 'delete-button');
 
@@ -45,9 +47,10 @@ function renderEntry() {
   $tdTwo.appendChild($pTwo);
   $pTwo.appendChild($textContentTwo);
   $tr.appendChild($tdThree);
-  $tdThree.appendChild($buttonEdit);
+  $tdThree.appendChild($row);
+  $row.appendChild($buttonEdit);
+  $row.appendChild($buttonDelete);
   $buttonEdit.appendChild($edit);
-  $tdThree.appendChild($buttonDelete);
   $buttonDelete.appendChild($delete);
 
   return $tr;
