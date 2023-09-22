c6cdd944818d4c395d6cac1a6775cb6743466c83
eric-k-chu <kiyuen88ec@gmail.com>
2023-09-22 19:08:09 +0000

    7

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const $addEvent = document.querySelector('.add-event');
 const $overlay = document.querySelector('.overlay');
+const $modalCancel = document.querySelector('#modal-cancel');
+const $modalConfirm = document.querySelector('#modal-confirm');
+const $modalTime = document.querySelector('#event-time option:checked');
+const $tableBody = document.querySelector('#table-body');
 
 $addEvent.addEventListener('click', function (event) {
-  $overlay.setAttribute('class', 'overlay');
-  console.log('hello');
+  $overlay.classList.toggle('hidden');
+});
+
+$modalCancel.addEventListener('click', function (event) {
+  $overlay.classList.toggle('hidden');
+});
+
+$modalConfirm.addEventListener('click', function (event) {
+
 });
